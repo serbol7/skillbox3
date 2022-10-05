8b36117a63c0ed4565265aa0767c029246c6e025
serbol7 <serbol@mail.ru>
2022-10-06 04:35:41 +0600

    more

--- src/data/products.js
+++ src/data/products.js
@@ -1,5 +1,85 @@
 export default [
   {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/457405e157f74b6b35cc6b45bcaf3b4d/aaba12ffd54b38fbe99ba290204cdf5780252f3acdf1304e14547485945d41a7.jpg",
+    title: "Тонометр B.Well MED-61 [на плечо, манжета от 220 мм до 420 мм ]",
+    price: 5990
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/34f0efe991ffd0f55c06b89f1a79e776/6cf6ef855ec01971d169950ddcd5bd7800b3951d5e9e1df0a950e346219626b7.jpg",
+    title: "Тонометр B.Well PRO-60 [на плечо, фонендоскоп, манжета от 220 мм до 420 мм ]",
+    price: 6990
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/f14ff6e36cc419240eecca7b7054d7e1/5f7266714b817619590ece3a25d49b57f1f2efc7f703063d294923bf4c2c7813.jpg",
+    title: "Тонометр B.Well MED-62 [на плечо, фонендоскоп, манжета от 220 мм до 420 мм ]",
+    price: 7390
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/50851496666f4889f4a139eae8f24716/e36766af1e8005fc9c64beab7e6e965fc3e4c24ad6b7fc4e676183bd90ac1f3e.jpg",
+    title: "Тонометр B.Well MED-63 [на плечо, фонендоскоп, манжета от 220 мм до 420 мм ]",
+    price: 7590
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/1c98d914bf08d616cb492e9603e97f92/bd0039b315a62a9e0870cc0620350cd3cd3a42ae1742e24308df363ed83926ec.jpg",
+    title: "Тонометр B.Well PRO-30 [на плечо, индикация аритмии, манжета от 220 мм до 320 мм ]",
+    price: 11490
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/50064497ad95ca3823ebcf4021f1d01c/3ca62af11eea1a2d35984a17864fd92f49db91f3078abf671e87d07d87dfaeb3.jpg",
+    title: "Тонометр B.Well PRO-39 [на запястье, индикация аритмии, манжета от 140 мм до 195 мм ]",
+    price: 12990
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/e1e14ff3e317a34b6b2ac1a8e83a12ef/464993c45c8d3db0146070878a1acc69576c048e972805f1faf3564daab6cbd5.jpg",
+    title: "Тонометр B.Well MED-57 [на запястье, индикация аритмии, манжета от 135 мм до 215 мм ]",
+    price: 15790
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/14a6fc663cf02fc62a060f6dd1075c4a/ad12f67cd089e1ce99e2050070e9b84ae19a5bcd86d5814ab4199ff866942089.jpg",
+    title: "Тонометр B.Well MED-53 [на плечо, индикация аритмии, манжета от 240 мм до 420 мм ]",
+    price: 19490
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/4774433e8ca0d53fd8ce2833e06cf1df/5dfa429ba7ebff13299cdc2537b707215b5a4fad6d66ed4f29e3a3c5d19b5bd2.jpg",
+    title: "Тонометр B.Well A-23 [на плечо, индикация аритмии, манжета от 220 мм до 360 мм ]",
+    price: 19990
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/60000e9029f65d2600654d29b3490941/b2152c89c2680d06beb803c7b566b8e684a48eb758ce6a8a8182ed345f165453.jpg",
+    title: "Весы DEXP SCGM-26P зеленый [электронные, материал - стекло, точность - 100 г, вес до - 180 кг]",
+    price: 3590
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/e69bd37a9d063578bf66164a17f02a95/9262272a12fea7022fa27166105cedab9138e655f3b64135af0f5762f10429b8.jpg",
+    title: "Весы DEXP SCM-30P черный [электронные, материал - стекло, точность - 100 г, вес до - 180 кг]",
+    price: 3590
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/f1e6f593c2d496f36badd03543f99463/62b936671f67303e14915143f5c08812c3a707e918e42f11f23a94d89627d234.jpg",
+    title: "Весы DEXP SC-20 серый [электронные, материал - стекло, точность - 100 г, вес до - 180 кг]",
+    price: 3990
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/7a77b20adfef2753c16eec7eab91bab2/45b3460d6c5333a003f968cb5ef973c80fbc86675b591a0a4a70dd82ff58748b.jpg",
+    title: "Весы DEXP SCI-30P серый [электронные, материал - стекло, точность - 100 г, вес до - 180 кг]",
+    price: 4190
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st1/fit/200/200/3eb549c27344c014c4413ff391dbe0d3/92341809b406c2998318fee45a15051cfdedd58cb14a9e6f4246140c51e58b88.jpg",
+    title: "Медицинский термометр B.Well WT-03 [белый, синий, аксиллярный, ректальный, оральный, точность +/- 0.1°C, автоотключение, дисплей, в футляре]",
+    price: 2090
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/71b4e3a0d8bb0ddb98d40315181b5fe2/d13f5533e2d7dfefe3cb28a40ce4059c14d91c87b589d836b573796777ff487a.jpg",
+    title: "Медицинский термометр B.Well WT-04 [белый, синий, аксиллярный, ректальный, оральный, точность +/- 0.1°C, автоотключение, дисплей]",
+    price: 2790
+  },
+  {
+    image: "https://c.dns-shop.kz/thumb/st4/fit/200/200/059ed36f26690771ed6106fbebebd5da/f73d05a9cc0d76876dd8b07c20bca4fc274daa1981afdbdea6cecbdc3393ae6f.jpg",
+    title: "Медицинский термометр B.Well WF-1000 [белый, синий, лобный, ушной, бесконтактный, точность +/-0.2°C, автоотключение, дисплей]",
+    price: 12990
+  },
+  {
     image: '/img/radio.jpg',
     title: 'Радионяня Motorola MBP16',
     price: 3690,
