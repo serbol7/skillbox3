1b6533d65eb24ddd3dd3339e9242fbb4d23f9666
serbol7 <serbol@mail.ru>
2022-10-06 04:00:45 +0600

    first
    first

---
+++ src/data.js
@@ -0,0 +1,3 @@
+const data = { message1: 'Hello. I am message number one!', message2: 'Hello. I am message number two!' };
+
+export default data;
---
+++ src/func.js
@@ -0,0 +1,5 @@
+function falert(p1) {
+  alert(p1);
+}
+
+export default falert;
